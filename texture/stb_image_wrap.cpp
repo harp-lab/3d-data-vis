e145bc0c5407810c2abd2440a2a2388541e56e2a
KokoFan16 <koko14fan@gmail.com>
2019-10-29 21:22:37 -0500

    shaders

---
+++ texture/stb_image_wrap.cpp
@@ -0,0 +1,10 @@
+//
+//  stb_image_wrap.cpp
+//  openGL
+//
+//  Created by kokofan on 10/29/19.
+//  Copyright © 2019 koko Fan. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
