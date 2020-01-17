c0655fa671e92db5572e24e5967d8c937bb60ede
KokoFan16 <koko14fan@gmail.com>
2020-01-17 15:41:26 -0600

    Add files via upload

---
+++ 3Dobject/stb_image_wrap.cpp
@@ -0,0 +1,10 @@
+//
+//  stb_image_wrap.cpp
+//  volumn_rendering
+//
+//  Created by kokofan on 1/17/20.
+//  Copyright © 2020 koko Fan. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
